d1059121ecdec1c3f957e919a74ed5a816917c4c
konishi333 <jobhuntingkonishi3@gmail.com>
2023-06-09 18:08:42 +0900

    First Commit

---
+++ routes/user.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+
+// router.use(mylogger);
+
+router.get("/", mylogger, (req, res) => {
+  res.send("ユーザーです。");
+});
+
+router.get("/info", (req, res) => {
+  res.send("ユーザー情報です。");
+});
+
+router.get("/:id", (req, res) => {
+  res.send(`${req.params.id}のユーザー情報を取得しました`)
+});
+router.post("/:id", (req, res) => {
+  res.send(`${req.params.id}のユーザー情報を登録しました`)
+});
+router.delete("/:id", (req, res) => {
+  res.send(`${req.params.id}のユーザー情報を削除しました`)
+});
+
+//ミドルウェア
+function mylogger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+};
+
+module.exports = router;
